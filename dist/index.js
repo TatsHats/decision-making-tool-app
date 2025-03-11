172ff9381aeac7a5a8ac15981bf04ab9879a6df7
TatsHats <6642017@gmail.com>
2025-03-11 22:23:57 +0200

    feat: added buttons and styles for them. added function createAddOptionSection (Вт, мар 11, 2025 10:23:57 )

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,73 @@
 "use strict";
+// -------------- Application container, header and two sections -------------------
+// ------------------- (left with table, right with buttons) -------------------
 function createApp() {
-    const appContainer = createAppContainer();
-    const listSection = createListSection();
-    const buttonSection = createButtonSection();
-    appContainer.append(listSection);
-    appContainer.append(buttonSection);
-    document.body.append(appContainer);
-}
-function createAppContainer() {
     const appContainer = document.createElement('div');
     appContainer.classList.add('appContainer');
     const titleApp = document.createElement('h1');
     titleApp.textContent = 'Decision Making Tool';
     titleApp.classList.add('titleApp');
     appContainer.append(titleApp);
-    return appContainer;
+    const content = document.createElement('div');
+    content.classList.add('content');
+    appContainer.append(content);
+    const listSection = createListSection();
+    content.append(listSection);
+    const buttonSection = createButtonSection();
+    content.append(buttonSection);
+    document.body.append(appContainer);
 }
+// ------------------- Left - List Section -------------------
 function createListSection() {
     const listSection = document.createElement('div');
     listSection.classList.add('listSection');
-    const titleList = document.createElement('h1');
-    titleList.textContent = 'Образец потом заменить тайтл на таблицу';
-    listSection.append(titleList);
+    const taskTable = createTable();
+    listSection.append(taskTable);
+    const addOptionSection = createAddOptionSection();
+    listSection.append(addOptionSection);
     const startButton = document.createElement('button');
     startButton.textContent = 'Start';
+    startButton.classList.add('startButton', 'button');
     listSection.append(startButton);
     return listSection;
 }
+// createTable
+function createTable() {
+    const table = document.createElement('table');
+    table.classList.add('table');
+    return table;
+}
+// createAddOptionSection
+function createAddOptionSection() {
+    const addOptionSection = document.createElement('div');
+    const addOptionTitle = document.createElement('h3');
+    addOptionTitle.textContent = 'Add Option';
+    addOptionTitle.classList.add('title');
+    const addButton = document.createElement('button');
+    addButton.textContent = '+';
+    addButton.classList.add('addButton');
+    return addOptionSection;
+}
+// ------------------- Right - Button Section -------------------
 function createButtonSection() {
     const buttonSection = document.createElement('div');
     buttonSection.classList.add('buttonSection');
-    const titlebuttonSection = document.createElement('h1');
-    titlebuttonSection.textContent = 'Образец потом заменить тайтл на button';
-    buttonSection.append(titlebuttonSection);
+    const pasteButton = document.createElement('button');
+    pasteButton.textContent = 'paste';
+    pasteButton.classList.add('pasteButton', 'button');
+    buttonSection.append(pasteButton);
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'clear';
+    clearButton.classList.add('clearButton', 'button');
+    buttonSection.append(clearButton);
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'save';
+    saveButton.classList.add('saveButton', 'button');
+    buttonSection.append(saveButton);
+    const loadButton = document.createElement('button');
+    loadButton.textContent = 'load';
+    loadButton.classList.add('loadButton', 'button');
+    buttonSection.append(loadButton);
     return buttonSection;
 }
 createApp();
