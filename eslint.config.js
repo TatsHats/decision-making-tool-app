77c3a01d13733c43680217e7e8cc949e99dcfd1d
TatsHats <6642017@gmail.com>
2025-03-11 20:58:01 +0200

    feat: added sections and test text, configured localhost (Вт, мар 11, 2025 08:58:01 )

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,28 @@
-import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
-import prettier from "eslint-plugin-prettier";
-import importPlugin from "eslint-plugin-import";
-//import unicorn from "eslint-plugin-unicorn";
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import unicorn from 'eslint-plugin-unicorn';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 /** @type {import("eslint").FlatConfig[]} */
 export default [
+  unicorn.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
       parser: tsparser,
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        location: 'readonly',
+        fetch: 'readonly',
+        HTMLElement: 'readonly',
+      },
     },
 
     linterOptions: {
@@ -21,35 +31,35 @@ export default [
     },
 
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
       prettier: prettier,
       import: importPlugin,
-      //unicorn: unicorn,
+      jsdoc: jsdoc,
     },
-
-    /*extends: [
-      "plugin:unicorn/recommended",
-    ],*/
 
     rules: {
       ...js.configs.recommended.rules,
       ...tseslint.configs.recommended.rules,
-      "prettier/prettier": "error",
-      "no-debugger": "off",
-      "no-console": "off",
-      "class-methods-use-this": "off",
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/consistent-type-assertions": [
-        "error",
-        { "assertionStyle": "never" }
+      'prettier/prettier': 'error',
+      'no-debugger': 'off',
+      'no-console': 'off',
+      'class-methods-use-this': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
       ],
-      "@typescript-eslint/consistent-type-imports": "error",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": [
-        "error",
-        { "accessibility": "explicit", "overrides": { "constructors": "off" } }
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
       ],
-      "@typescript-eslint/member-ordering": "error"
+      '@typescript-eslint/member-ordering': 'error',
+      'jsdoc/require-returns-type': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/valid-types': 'error',
     },
   },
 ];
