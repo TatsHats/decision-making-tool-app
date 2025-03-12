e0677f069624717fad8a1ddeb1a6a01e789987ed
TatsHats <6642017@gmail.com>
2025-03-12 21:28:33 +0200

    feat: save before splitting modules (Ср, мар 12, 2025 09:28:33 )

--- dist/index.js
+++ dist/index.js
@@ -30,6 +30,30 @@ function createListSection() {
 function createTable() {
     const table = document.createElement('table');
     table.classList.add('table');
+    const tableHead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    const idTask = document.createElement('th');
+    idTask.textContent = 'ID';
+    const titleTask = document.createElement('th');
+    titleTask.textContent = 'Task title';
+    const weightTask = document.createElement('th');
+    weightTask.textContent = 'Task weight';
+    headerRow.append(idTask, titleTask, weightTask);
+    tableHead.append(headerRow);
+    table.append(tableHead);
+    const tableBody = document.createElement('tbody');
+    for (let i = 0; i < 10; i++) {
+        const row = document.createElement('tr');
+        const idCell = document.createElement('td');
+        const titleCell = document.createElement('td');
+        const weightCell = document.createElement('td');
+        idCell.textContent = '';
+        titleCell.textContent = '';
+        weightCell.textContent = '';
+        row.append(idCell, titleCell, weightCell);
+        tableBody.appendChild(row);
+    }
+    table.append(tableBody);
     return table;
 }
 // createAddOptionSection
@@ -51,19 +75,19 @@ function createButtonSection() {
     const buttonSection = document.createElement('div');
     buttonSection.classList.add('buttonSection');
     const pasteButton = document.createElement('button');
-    pasteButton.textContent = 'paste';
+    pasteButton.textContent = 'Paste list';
     pasteButton.classList.add('pasteButton', 'button');
     buttonSection.append(pasteButton);
     const clearButton = document.createElement('button');
-    clearButton.textContent = 'clear';
+    clearButton.textContent = 'Clear list';
     clearButton.classList.add('clearButton', 'button');
     buttonSection.append(clearButton);
     const saveButton = document.createElement('button');
-    saveButton.textContent = 'save';
+    saveButton.textContent = 'Save list to file';
     saveButton.classList.add('saveButton', 'button');
     buttonSection.append(saveButton);
     const loadButton = document.createElement('button');
-    loadButton.textContent = 'load';
+    loadButton.textContent = 'Load list from file';
     loadButton.classList.add('loadButton', 'button');
     buttonSection.append(loadButton);
     return buttonSection;
