1e2f14adc8ed1dd0f6db456dcc312c1a930f767a
TatsHats <6642017@gmail.com>
2025-03-13 11:23:04 +0200

    feat: changed styles for fixing the application and table, list-of-buttons module added (Чт, мар 13, 2025 11:23:04 )

---
+++ src/components/list-of-buttons.ts
@@ -0,0 +1,27 @@
+// ------------------- Right - Button Section -------------------
+export function createButtonSection(): HTMLElement {
+    const buttonSection = document.createElement('div');
+    buttonSection.classList.add('buttonSection');
+  
+    const pasteButton = document.createElement('button');
+    pasteButton.textContent = 'Paste list';
+    pasteButton.classList.add('pasteButton', 'button');
+    buttonSection.append(pasteButton);
+  
+    const clearButton = document.createElement('button');
+    clearButton.textContent = 'Clear list';
+    clearButton.classList.add('clearButton', 'button');
+    buttonSection.append(clearButton);
+  
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save list to file';
+    saveButton.classList.add('saveButton', 'button');
+    buttonSection.append(saveButton);
+  
+    const loadButton = document.createElement('button');
+    loadButton.textContent = 'Load list from file';
+    loadButton.classList.add('loadButton', 'button');
+    buttonSection.append(loadButton);
+  
+    return buttonSection;
+  }
