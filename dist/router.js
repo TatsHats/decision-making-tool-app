75ef838521506c6eca8a33a34797410c26390e59
TatsHats <6642017@gmail.com>
2025-03-14 16:51:25 +0200

    feat: added createDecisionPicker function. files app and list-of-buttons deleted, code moved to list-of-options. added router file for chenge pages (Пт, мар 14, 2025 04:51:25 )

---
+++ dist/router.js
@@ -0,0 +1,6 @@
+var Page;
+(function (Page) {
+    Page["List"] = "list";
+    Page["Picker"] = "picker";
+})(Page || (Page = {}));
+export function changePage(page) { }
--- src/components/decision-picker.ts
+++ src/components/decision-picker.ts
@@ -1,1 +1,41 @@
-export {};
+import { createInputElement } from './list-of-options.js';
+
+export function createDecisionPicker(): HTMLElement {
+  const sectionPicker = document.createElement('div');
+  sectionPicker.classList.add('sectionPicker');
+
+  const titleApp = document.createElement('h1');
+  titleApp.textContent = 'Decision Making Tool';
+  titleApp.classList.add('titleApp');
+
+  const sectionPickerSettings = document.createElement('div');
+  sectionPickerSettings.classList.add('sectionPickerButtons');
+
+  const wheelCanvas = document.createElement('canvas');
+  wheelCanvas.classList.add('wheelCanvas');
+
+  // Buttons
+  const backButton = document.createElement('button');
+  backButton.textContent = 'Back';
+  backButton.addEventListener('click', () => {});
+
+  const soundButton = document.createElement('button');
+  backButton.textContent = 'Sound';
+  backButton.addEventListener('click', () => {});
+
+  const spinButton = document.createElement('button');
+  spinButton.textContent = 'Spin';
+  spinButton.addEventListener('click', () => {});
+
+  // Input time
+  const duration = createInputElement('number', 'sec', '10');
+
+  const infoField = document.createElement('span');
+  backButton.textContent = 'Press stat button';
+  infoField.classList.add('infoField');
+
+  sectionPickerSettings.append(backButton, soundButton, spinButton, duration);
+  sectionPicker.append(titleApp, sectionPickerSettings, infoField, wheelCanvas);
+
+  return sectionPicker;
+}
